ca23e78b14c8ccc650cf5c55d32f5baef3719e01
vpuzin <vlad.puzin@gmail.com>
2020-02-29 19:03:42 +0300

    initial

---
+++ scripts/login.js
@@ -0,0 +1,47 @@
+let loginButton = document.querySelector('.login-button');
+const loginUrl = 'http://127.0.0.1:8000/api-token-auth/';
+let failWindow = document.querySelector('.on-fail-login');
+
+document.querySelectorAll('input').forEach(function (x) {
+    x.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        failWindow.style.display = 'none';
+    })
+});
+
+loginButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let email = document.querySelector('#inputEmail').value;
+    let password = document.querySelector('#inputPassword').value;
+
+    loginButton.innerHTML = `<img src="../icons/Rolling-0.4s-217px.svg" alt="Loading . . ." width="50" height="30">`;
+    postLogin(email, password)
+        .then(function (data) {
+            if (data['status'] === 'success') {
+                location.href = 'views/welcome.html?id=' + data['data']['user_id'];
+            } else {
+                failWindow.style.display = 'block';
+            }
+            loginButton.innerHTML = 'Sign in';
+        });
+});
+
+function postLogin(email, password) {
+    return fetch(loginUrl, {
+        accept: 'application/json',
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+    })
+        .then(response => response.json())
+}
---
+++ scripts/scripts.js
@@ -0,0 +1,92 @@
+let serverUrl = `http://127.0.0.1:8000`;
+
+function initMap() {
+    var uluru = {lat: 54.1973332, lng: 45.1052696};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: {
+            lat: 54.1973332,
+            lng: 45.1052696
+        }
+    });
+
+    document.querySelector('.add-event-button').addEventListener('click', function (x) {
+        x.preventDefault();
+        fetch(`${serverUrl}/events/create-event/`, {
+            accept: 'application/json',
+            method: 'POST',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: 'MockName',
+                description: 'MockDesc',
+                event_date: new Date(),
+                latitude: getRandomInt(100),
+                longitude: getRandomInt(100),
+            }),
+        })
+            .then(response => response.json())
+            .then(data => console.info(data))
+    });
+
+    let url = new URL(window.location.href);
+    let userId = url.searchParams.get('id');
+    fetch(`${serverUrl}/user/get-by-id/${userId}`, {
+        cache: 'no-cache',
+    })
+        .then((response) => response.json())
+        .then(
+            (response) => {
+                fillPageByUserInfo(response['data'].user);
+                fetch(`${serverUrl}/events/get-list`,{
+                    cache: 'no-cache'
+                })
+                    .then(response => response.json())
+                    .then(response => {
+                        fillEventsList(response['data'], map)
+                    })
+            }
+        );
+}
+
+function fillEventsList(events, map) {
+    let eventsBox = document.querySelector('.event-box');
+    events.forEach(x => {
+        const marker = new google.maps.Marker({
+            position: {lat: (parseFloat(x.latitude)), lng: (parseFloat(x.longitude))},
+            map: map,
+        });
+
+        let eventNode = document.createElement('div');
+        eventNode.className = 'event';
+        eventNode.innerHTML = `<div class="event-info-bar">
+                    <h3 class="event-title">${x.name}</h3>
+                    <span class="event-description">${x.description}</span>
+                </div>
+                <button class="btn btn-danger take-part" data-id='${x.id}'>Take part!</button>`;
+
+        eventsBox.appendChild(eventNode);
+    });
+
+    setTimeout(setListeners, 2000);
+}
+
+function setListeners() {
+    document.querySelectorAll('.take-part').forEach(function (x) {
+        x.addEventListener('click', function (event) {
+            // console.log(event.target);
+            event.preventDefault();
+        });
+    });
+}
+
+function  fillPageByUserInfo(user) {
+    document.querySelector('.you-logged-as').innerHTML = 'You logged as ' + user.username;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
